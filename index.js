875032e8c8703857c3cb4e35c5c47541dca6f1b9
Julio Silva de Castro <Julio.Castro@hprodomain.com.br>
2024-09-23 07:32:55 -0300

    M01A09

--- index.js
+++ index.js
@@ -10,19 +10,40 @@ server.use(express.json());
 
 const cursos = ['Node JS', 'JavaScript', 'Angular'];
 
+// GET - Pegando todos os registros
 server.get('/cursos', (req, res) => {
   return res.json(cursos);
 });
 
+// GET - Pegando registro específico
 server.get('/cursos/:index', (req, res) => {
   const { index } = req.params;
 
   return res.json(`${cursos[index]}`);
 });
 
+// POST - Adicionando registro
 server.post('/cursos', (req, res) => {
   const { name } = req.body;
   cursos.push(name);
+
+  return res.json(cursos);
+});
+
+// PUT - Editando registro
+server.put('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos);
+});
+
+server.delete('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+
+  cursos.splice(index, 1);
 
   return res.json(cursos);
 });
